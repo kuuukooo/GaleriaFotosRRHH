d5918a43c45f31c449e5cd0b0b7052eb28579579
José Ramírez <jwh743@gmail.com>
2023-10-09 14:49:10 -0300

    Se agregó la función de cargar imágenes (pendiente arreglo de backdrops)

---
+++ buscar_img.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#search-form").on("submit", function(e) {
+        e.preventDefault(); // Evita que el formulario se envíe de manera tradicional
+
+        var searchTerm = $("#search-input").val();
+
+        $.ajax({
+            url: "buscar_img.php",
+            method: "POST",
+            data: { search: searchTerm },
+            dataType: "json",
+            success: function(data) {
+                if (data.error) {
+                    // Manejar el caso de que no se encontraron resultados
+                    console.log(data.error);
+                } else {
+                    // Manejar el caso de que se encontró una imagen
+                    console.log("Imagen encontrada:", data);
+
+                    // Aquí puedes mostrar la imagen en el lugar adecuado en tu página
+                    // Puedes usar el ID de la imagen para identificarla en el DOM y mostrarla
+                    var imageId = data.id_imagen;
+                    var imageContainer = $("#col" + imageId); // Asegúrate de que este selector sea correcto
+
+                    // Luego, puedes manipular imageContainer para mostrar la imagen
+                    // Por ejemplo, puedes cambiar la fuente de una etiqueta <img>
+                }
+            },
+            error: function(xhr, status, error) {
+                console.log('Error en la solicitud AJAX:');
+                console.log('Status:', status);
+                console.log('Error:', error);
+            }
+        });
+    });
+});
--- index.php
+++ index.php
@@ -1,39 +1,25 @@
 <!-- Inicio de la sesión de PHP -->
 <?php
-session_start();
-require "./controllers/posts.php";
+    session_start();
+    require "./controllers/posts.php";
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: login.php'); // Redirige al usuario si no está autenticado
+        exit();
+    }
+    require "./database/database.php";
+    $user_id = $_SESSION['user_id'];
+    $query = "SELECT Usuario FROM usuarios WHERE id_usuario = :user_id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
 
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php'); // Redirige al usuario si no está autenticado
-    exit();
-}
-
-require "./database/database.php";
-$user_id = $_SESSION['user_id'];
-$query = "SELECT Usuario FROM usuarios WHERE id_usuario = :user_id";
-$stmt = $conn->prepare($query);
-$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-$stmt->execute();
-
-if ($stmt->rowCount() > 0) {
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $nombre_usuario = $row['Usuario'];
-} else {
-    $nombre_usuario = "Usuario desconocido";
-}
-
-$por_pagina = 12;
-
-if (isset($_GET['pagina'])) {
-    $pagina_actual = $_GET['pagina'];
-} else {
-    $pagina_actual = 1;
-}
-
-$empieza = ($pagina_actual - 1) * $por_pagina;
-$query = "SELECT * FROM imagenes_sueltas ORDER BY id_imagen DESC LIMIT $empieza, $por_pagina";
-$stmt = $conn->prepare($query);
-$stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $nombre_usuario = $row['Usuario'];
+    } else {
+        $nombre_usuario = "Usuario desconocido";
+    }
+    $pagina_actual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
 ?>
 <!-- Inicio del Html -->
 <!DOCTYPE html>
@@ -44,12 +30,10 @@ $stmt->execute();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria de fotos A.J</title>
     <link rel="icon" type="image/x-icon" href="logovierciazul.svg">
-    <!-- Librerías -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js"></script>
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -58,13 +42,6 @@ $stmt->execute();
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="estilos.css"></link>
     <link rel="stylesheet" type="text/css" href="navbar2.css"></link>
-    <style>
-    /* CSS para limitar el tamaño máximo de las imágenes en el carrusel */
-    .carousel-inner img {
-        max-width: 100%;
-        height: auto;
-    }
-</style>
 </head>
 <body>
 <div class="container">
@@ -170,59 +147,58 @@ $stmt->execute();
         </div>
     </div>
 
+<!-- Card Bodies y Botones de utilidad -->
 
-<!-- Imágenes -->
-<div class="container mt-5 mb-5 py-2" id="image-container">
+<div class="container mt-5 mb-5 py-2">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mb-5 justify-content-center">
         <?php
-        $stmt = $conn->prepare($query);
-        $stmt->execute();
-        while ($post = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            // Divide la cadena de imágenes en un array
+        $conn = mysqli_connect("localhost", "root", "", "galeria");
+        $por_pagina = 12;
+
+        if (isset($_GET['pagina'])) {
+            $pagina = $_GET['pagina'];
+        } else {
+            $pagina = 1;
+        }
+
+        $empieza = ($pagina - 1) * $por_pagina;
+        $query = "SELECT * FROM imagenes_sueltas ORDER BY id_imagen DESC LIMIT $empieza, $por_pagina"; // Cambia el ORDER BY
+        $resultado = mysqli_query($conn, $query);
+
+        while ($post = mysqli_fetch_assoc($resultado)) {
             $imagesName = explode(",", $post['imagen']);
         ?>
-        <div class="col">
-            <div class="card">
-                <div class="carousel slide" id="carousel<?=$post['id_imagen']?>" data-bs-ride="false">
-                    <div class="carousel-inner">
-                    <?php foreach ($imagesName as $j => $imageName) {
-                            $isActive = $j == 0 ? 'active' : '';
-                            ?>
-                        <div class="carousel-item <?= $isActive ?>">
+        <div class="col" style="width: 275px">
+            <div class="carousel slide" id="carousel<?=$post['id_imagen'] ?>" data-bs-ride="false">
+                <div class="carousel-inner">
+                    <?php for($j = 0; $j < count($imagesName); $j++) {
+                        $isActive = $j == 0 ? 'active' : '';
+                    ?>
+                    <div class="carousel-item <?= $isActive ?>">
                         <a href="#" data-bs-toggle="modal" data-bs-target="#modal<?=$post['id_imagen']?>-<?=$j?>">
-                                <!-- Agrega un atributo data-description con la descripción de la imagen -->
-                                <div class="image-container" id="image-<?=$post['id_imagen']?>-<?=$j?>" data-description="<?= $descriptionsArray[$j] ?>" style="background-image: url('./assets/images/posts/<?= $imageName ?>');"></div>
-                            </a>
-                        </div>
+                            <div class="image-container" style="background-image: url('./assets/images/posts/<?= $imagesName[$j] ?>');"></div>
+                        </a>
+                    </div>
                     <?php } ?>
-                    </div>
                 </div>
-
-                <div class="card-body" id="card-body-<?=$post['id_imagen']?>">
-                    <div class="original-description">
-                        <?= $post['descripcion'] ?>
-                    </div>
-            <div class="botones-utilidades">
-                <button class="delete-button" data-image-id="<?=$post['id_imagen']?>"><i class="bi bi-trash3 fa-6x"></i></button>
-                <button class="btn-edit-description" data-image-id="<?=$post['id_imagen']?>"><i class="bi bi-pencil-square"></i></button>
-                
-                <!-- Botón de descarga -->
-                <?php
-                    // Crear una cadena de descripciones separadas por comas
-                    $descriptionsArray = explode(",", $post['descripcion']);
-                    $descriptionsString = implode(',', $descriptionsArray);
-                ?>
-                <a href="#" class="download-button" 
-                   data-images="<?= implode(',', $imagesName) ?>" 
-                   data-description="<?= $post['descripcion'] ?>" 
-                   data-descriptions="<?= $descriptionsString ?>">
-                    <i class="bi bi-download"></i>
-                </a>
             </div>
-        </div>
 
-        <!-- Agrega el formulario de edición de descripción aquí -->
-        <div class="description-edit-container" id="description-edit-<?=$post['id_imagen']?>" style="display: none;">
+            <div class="card mt-2">
+                <div class="card-body">
+                    <div class="original-description">
+                     <?php if (!isset($_GET['edit-description']) || $_GET['edit-description'] != $post['id_imagen']) { ?>
+                        <?= $post['descripcion'] ?>
+                        </div>
+                        <div class="botones-utilidades">
+                            <a href="eliminar-imagen.php?id_imagen=<?=$post['id_imagen']?>&index=<?=$j?>"><i class="bi bi-trash3"></i></a>
+                            <button class="btn-edit-description" data-image-id="<?=$post['id_imagen']?>"><i class="bi bi-pencil-square"></i></button>
+                            <a href="./assets/images/posts/<?= $imagesName[$j] ?>" download><i class="bi bi-download"></i></a>
+                        </div>
+                        <?php } ?>
+                </div>
+            </div>
+            
+            <div class="description-edit-container" id="description-edit-<?=$post['id_imagen']?>" style="display: none;">
             <form action="editar-descripcion.php" method="POST" id="edit-form-<?= $post['id_imagen'] ?>">
                 <textarea maxlength="25" name="new-description" class="form-control"><?= $post['descripcion'] ?></textarea>
                 <input type="hidden" name="id_imagen" value="<?= $post['id_imagen'] ?>">
@@ -231,23 +207,20 @@ $stmt->execute();
                 <button type="submit" class="btn btn-primary">Guardar</button>
                 <a href="#" class="btn btn-secondary cancel-edit" data-image-id="<?= $post['id_imagen'] ?>">Cancelar </a>
             </form>
+            </div>
         </div>
+        <?php } ?>
     </div>
 </div>
-<?php } ?>
-
-
-
 
 
 <!-- Paginación -->
-<section class="paginación">
+
 <div class="pagination">
     <?php
     $query_total = "SELECT COUNT(*) as total FROM imagenes_sueltas";
-    $stmt_total = $conn->prepare($query_total);
-    $stmt_total->execute();
-    $row_total = $stmt_total->fetch(PDO::FETCH_ASSOC);
+    $resultado_total = mysqli_query($conn, $query_total);
+    $row_total = mysqli_fetch_assoc($resultado_total);
 
     $total_imagenes = $row_total['total'];
     $total_paginas = ceil($total_imagenes / $por_pagina);
@@ -274,19 +247,18 @@ $stmt->execute();
         echo "<a href='index.php?pagina=" . ($pagina_actual + 1) . "' class='btn btn-outline-primary'>&gt;</a>";
     }
     ?>
-</div>
-</section>
 
+</div>
 
 
 
 <!-- Modales -->
 <?php
-$stmt = $conn->prepare($query); // Usar la misma consulta con PDO
-$stmt->execute(); // Ejecutar la consulta
-while ($post = $stmt->fetch(PDO::FETCH_ASSOC)) {
+$resultado = mysqli_query($conn, $query); // Reiniciar el resultado
+while ($post = mysqli_fetch_assoc($resultado)) {
     $imagesName = explode(",", $post['imagen']);
-    foreach ($imagesName as $j => $imageName) { ?>
+    foreach ($imagesName as $j => $imageName) {
+?>
 <!-- Código del Modal -->
 <div class="modal fade" id="modal<?=$post['id_imagen']?>-<?=$j?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg">
@@ -331,105 +303,7 @@ while ($post = $stmt->fetch(PDO::FETCH_ASSOC)) {
 <!-- Script de Botones -->
 
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function() {
-    // Función para guardar el estado del modo en el almacenamiento local
-    function saveDarkModeState(isDarkMode) {
-        localStorage.setItem('darkMode', isDarkMode);
-    }
-
-    // Función para cargar el estado del modo desde el almacenamiento local
-    function loadDarkModeState() {
-        const isDarkMode = localStorage.getItem('darkMode') === 'true';
-        if (isDarkMode) {
-            $("body").addClass("dark");
-            $("#darkModeSwitch").prop("checked", true);
-        }
-    }
-
-    // Cargar el estado del modo al cargar la página
-    loadDarkModeState();
-
-    // Manejar el cambio de modo
-    $("#darkModeSwitch").click(function() {
-        const isDarkMode = $("body").hasClass("dark");
-        $("body").toggleClass("dark");
-        $("#darkModeSwitch").prop("checked", !isDarkMode);
-        // Guardar el estado del modo en el almacenamiento local
-        saveDarkModeState(!isDarkMode);
-        // Redirigir a la misma página con el estado del modo como parámetro en la URL
-        const currentPage = window.location.href;
-        const newUrl = currentPage + (currentPage.includes("?") ? "&" : "?") + "darkMode=" + (!isDarkMode ? "1" : "0");
-        window.location.href = newUrl;
-    });
-
-    // Manejar la paginación
-    $(".pagination a").click(function(e) {
-        e.preventDefault(); // Evitar que el enlace navegue a otra página
-
-        var pagina = $(this).text(); // Obtener el número de página
-        // Pasar el estado del modo actual como parámetro en la URL al cambiar de página
-        const isDarkMode = $("body").hasClass("dark") ? "1" : "0";
-        window.location.href = "index.php?pagina=" + pagina + "&darkMode=" + isDarkMode; // Redirigir a la página correspondiente
-    });
-});
-// Manejar la paginación
-$(".pagination a").click(function(e) {
-    e.preventDefault(); // Evitar que el enlace navegue a otra página
-
-    var pagina = $(this).text(); // Obtener el número de página
-    $.ajax({
-        url: "./cargar_imagenes.php",
-        method: "GET",
-        data: { pagina: pagina },
-        success: function(data) {
-            // Actualiza el contenido del contenedor de imágenes con el nuevo contenido
-            $("#image-container").html(data);
-        }
-    });
-});
-});
-$(".delete-button").click(function(event) {
-        event.preventDefault(); // Evitar que el enlace navegue a otra página
-
-        const imageId = $(this).data("image-id");
-
-        console.log("Eliminando la imágen con el id: " + imageId)
-
-        $.ajax({
-            url: "eliminar-imagen.php",
-            method: "POST",
-            data: { id_imagen: imageId },
-            dataType: "json",
-            success: function(response) {
-                if (response.success) {
-                    // Eliminación exitosa, puedes mostrar un mensaje de éxito en la página
-                    alert("Imagen eliminada exitosamente.");
-                    // Ocultar tanto el image-container como el card-body específicos
-                    $(`#image-${imageId}`).hide();
-                    $(`#card-body-${imageId}`).hide();
-                } else {
-                    // Error al eliminar, puedes mostrar un mensaje de error en la página
-                    alert("Error al eliminar la imagen: " + response.error);
-                }
-            },
-            error: function(xhr, status, error) {
-                // Manejar errores de la solicitud AJAX aquí
-                console.error(error);
-            }
-        });
-    });
-
-
-</script>
-<script src="descarga_imágenes.js"></script>
-
-<!-- Función para editar la descripción -->
-
-<script>
-$(document).ready(function() {
-    console.log("Script de edición de descripción cargado."); // Agrega este console.log para verificar si el script se carga
-
     $(".btn-edit-description").click(function() {
         console.log("Botón de edición de descripción clickeado");
 
@@ -465,7 +339,149 @@ $(document).ready(function() {
     });
 });
 </script>
-<script src="editar-descripcion.php"></script>
-<script src="eliminar-imagen.php"></script>
+
+<!-- Script de Modo Oscuro y Claro --> 
+
+<script>
+  // Función para establecer una cookie con un nombre, valor y duración
+  function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  }
+
+  // Función para obtener el valor de una cookie por nombre
+  function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + '=')) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+
+  const modeText = document.querySelector(".mode-text");
+  const darkModeSwitch = document.getElementById("darkModeSwitch");
+  const body = document.body;
+  let darkModeEnabled = false;
+
+  // Función para cambiar el modo y guardar en cookies
+  function toggleDarkMode() {
+    darkModeEnabled = !darkModeEnabled;
+    body.classList.toggle("dark");
+    updateModalBackground();
+    updateCardBodyBackground();
+
+    if (body.classList.contains("dark")) {
+      modeText.innerText = "Modo Claro";
+      setCookie("darkMode", "enabled", 365);
+    } else {
+      modeText.innerText = "Modo Oscuro";
+      setCookie("darkMode", "disabled", 365);
+    }
+  }
+
+  // Comprueba el estado del modo oscuro al cargar la página
+  document.addEventListener("DOMContentLoaded", () => {
+    const darkMode = getCookie("darkMode");
+
+    if (darkMode === "enabled") {
+      body.classList.add("dark");
+      darkModeEnabled = true;
+      updateModalBackground();
+      updateCardBodyBackground();
+      modeText.innerText = "Modo Claro";
+    } else if (darkMode === "disabled") {
+      body.classList.remove("dark");
+      updateModalBackground();
+      updateCardBodyBackground();
+      modeText.innerText = "Modo Oscuro";
+    }
+
+    // Evita la animación al recargar la página
+    body.classList.add("initial-dark-mode");
+    setTimeout(() => {
+      body.classList.remove("initial-dark-mode");
+    }, 0);
+
+    if (!darkModeEnabled) {
+      body.classList.remove("dark");
+      setTimeout(() => {
+        body.classList.add("dark");
+        updateModalBackground();
+        updateCardBodyBackground();
+      }, 100);
+    }
+  });
+
+  // Agrega un event listener al interruptor de modo oscuro
+  darkModeSwitch.addEventListener("click", () => {
+    toggleDarkMode();
+  });
+
+  // Función para actualizar el fondo de las cajas de modales
+  function updateModalBackground() {
+    const modalBodies = document.querySelectorAll(".modal-body");
+    modalBodies.forEach((modalBody) => {
+      if (darkModeEnabled) {
+        modalBody.style.backgroundColor = "#18191a";
+      } else {
+        modalBody.style.backgroundColor = "#fff";
+      }
+    });
+  }
+
+  // Función para cambiar el color de fondo de los elementos .card-body
+  function updateCardBodyBackground() {
+    const cardBodies = document.querySelectorAll(".card-body");
+    cardBodies.forEach((cardBody) => {
+      if (darkModeEnabled) {
+        cardBody.style.backgroundColor = "#18191a";
+      } else {
+        cardBody.style.backgroundColor = "#fff";
+      }
+    });
+  }
+
+  // Función para cambiar la barra lateral (sidebar)
+  function toggleSidebar() {
+    const sidebar = document.querySelector('nav');
+    const mainContent = document.querySelector(".gallery-container");
+    sidebar.classList.toggle("close");
+    updateMargin();
+
+    // Guarda el estado de la barra lateral en cookies
+    const sidebarState = sidebar.classList.contains("close") ? "closed" : "open";
+    setCookie("sidebarState", sidebarState, 365);
+  }
+
+  // Event listener para el botón de alternar la barra lateral
+  toggle.addEventListener("click", () => {
+    toggleSidebar();
+  });
+
+  // Función para actualizar el margen del contenido principal
+  function updateMargin() {
+    const sidebar = document.querySelector('nav');
+    const mainContent = document.querySelector(".gallery-container");
+    if (sidebar.classList.contains("close")) {
+      mainContent.style.marginLeft = '0';
+    } else {
+      mainContent.style.marginLeft = '250px';
+    }
+  }
+
+  // Llama a applySidebarState() después de cargar la página para establecer el estado de la barra lateral
+  function applySidebarState() {
+    const sidebarState = getCookie("sidebarState");
+    if (sidebarState === "open") {
+      toggleSidebar();
+    }
+  }
+  applySidebarState();
+</script>
+<script src="navbar2.js"></script>
 </body>
 </html>
