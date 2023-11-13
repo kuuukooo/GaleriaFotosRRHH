62c089330a58f237dabe6862fff4c4fe50ca5e0e
José Ramírez <jwh743@gmail.com>
2023-11-13 13:36:38 -0300

    se añadieron optimizaciones para eliminar imágenes entre páginas

--- eliminar_imagen.js
+++ eliminar_imagen.js
@@ -10,19 +10,30 @@ $(".delete-button").click(function() {
         dataType: "json", // Esperar una respuesta JSON
         success: function(response) {
             if (response.success) {
-                // Eliminación exitosa, actualizar la vista aquí si es necesario
+                // Eliminación exitosa, actualizar la vista aquí
                 console.log("Imagen eliminada con éxito.");
-                console.log(response.success);
+
+                // Encuentra y elimina visualmente el contenedor de la imagen
+                $(`#col${imageId}`).remove();
+
+                // Puedes llamar a la función que maneja la paginación o recarga dinámica
+                // para asegurarte de que la galería se actualice correctamente
+                cargarImagenesYActivarEdicion(1); // Ajusta el número de página si es necesario
+
+                // Muestra un mensaje de éxito para el usuario
+                alert("Imagen eliminada exitosamente.");
             } else {
-                // Error al eliminar, manejar el error aquí si es necesario
+                // Manejar errores de eliminación, si es necesario
                 console.error("Error al eliminar la imagen.");
                 console.error(response.error);
+                alert("Error al eliminar la imagen: " + response.error);
             }
         },
         error: function(xhr, status, error) {
             // Manejar errores de la solicitud AJAX aquí
             console.error("Error en la solicitud AJAX.");
             console.error(error);
+            alert("Error en la solicitud AJAX. Por favor, inténtalo de nuevo.");
         }
     });
 });
