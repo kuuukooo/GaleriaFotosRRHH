d15466c49e041248fdc80995bfa614247fa76838
José Ramírez <jwh743@gmail.com>
2023-11-13 13:39:28 -0300

    Se añadió la funcionalidad de la paginación en el index

--- index2.php
+++ index2.php
@@ -21,6 +21,22 @@ if ($stmt->rowCount() > 0) {
 } else {
     $nombre_usuario = "Usuario desconocido";
 }
+$por_pagina = 12;
+$pagina_actual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+
+// Ajustamos la lógica para obtener el valor de $empieza
+if ($pagina_actual > 1) {
+    $empieza = ($pagina_actual - 1) * $por_pagina;
+} else {
+    $empieza = 0;
+}
+
+// Usamos una variable adicional para llevar registro de la página actual
+$pagina_mostrada = $pagina_actual;
+
+$query = "SELECT * FROM imagenes_sueltas ORDER BY id_imagen DESC LIMIT " . intval($empieza) . ", $por_pagina";
+$stmt = $conn->prepare($query);
+$stmt->execute();
 ?>
 <!-- Inicio del Html -->
 <!DOCTYPE html>
@@ -165,8 +181,12 @@ if ($stmt->rowCount() > 0) {
                 </div>
             </div>
 
-            <!-- Aquí solía estar la paginacion -->
-            
+            <!-- Paginación -->
+            <div id="pagination-container">
+            <ul class="pagination">
+                <!-- Los botones de paginación se generarán aquí -->
+            </ul>
+            </div>
 
 <!-- Script de Botones -->
 
