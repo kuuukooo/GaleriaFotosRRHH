fb2571c24857884ddce99ae8bba475c241646c50
José Ramírez <jwh743@gmail.com>
2023-11-14 09:01:25 -0300

    se añadió la funcionalidad para detectar el usuario logeado en la cookie

--- login/controladorLogin.php
+++ login/controladorLogin.php
@@ -3,15 +3,13 @@ session_start();
 
 require "../database/database.php";
 
-
-
 if (!empty($_POST["btningresar"])) {
     if (empty($_POST["usuario"]) || empty($_POST["password"])) {
         $_SESSION["mensaje"] = 'Complete los campos';
     } else {
         $usuario = $_POST['usuario'];
         $clave = $_POST['password'];
-        
+
         try {
             // Crea una instancia de la clase Database
             $database = new Database();
@@ -19,40 +17,36 @@ if (!empty($_POST["btningresar"])) {
             // Obtiene la conexión
             $conex = $database->getConnection();
 
-            // Realizar la consulta a la base de datos para autenticar al usuario
-            $sql = $conex->prepare("SELECT id_usuario FROM usuarios WHERE usuario=? AND contrasena=?");
+            // Realizar la consulta a la base de datos para autenticar al usuario y obtener el tipo de usuario
+            $sql = $conex->prepare("SELECT id_usuario, tipo_usuario, usuario FROM usuarios WHERE usuario=? AND contrasena=?");
             $sql->bindParam(1, $usuario, PDO::PARAM_STR);
             $sql->bindParam(2, $clave, PDO::PARAM_STR);
             $sql->execute();
-            $user_id = $sql->fetchColumn();
+            $result = $sql->fetch(PDO::FETCH_ASSOC);
 
-            if ($user_id) {
+            if ($result) {
                 // Autenticación exitosa
-                $_SESSION["user_id"] = $user_id; // Almacena el ID del usuario en la sesión
-            
-                // Realiza una consulta adicional para obtener el tipo de usuario
-                $sql = $conex->prepare("SELECT tipo_usuario FROM usuarios WHERE id_usuario=?");
-                $sql->bindParam(1, $user_id, PDO::PARAM_INT);
-                $sql->execute();
-                $tipo_usuario = $sql->fetchColumn();
-            
-                if ($tipo_usuario) {
-                    $_SESSION["tipo_usuario"] = $tipo_usuario;
-            
-                    // Almacena el tipo de usuario en una cookie
-                    setcookie("tipo_usuario", $tipo_usuario, time() + (86400 * 30), "/"); // Caduca en 30 días
-            
-                    switch ($tipo_usuario) {
-                        case "Admin":
-                            header("Location: ../index2.php");
-                            break;
-                        case "Usuario":
-                            header("Location: ../index2.php");
-                            break;
-                        default:
-                            header("Location: ../index2.php"); 
-                            break;
-                    }
+                $_SESSION["user_id"] = $result["id_usuario"]; // Almacena el ID del usuario en la sesión
+
+                $tipo_usuario = $result["tipo_usuario"];
+                $_SESSION["tipo_usuario"] = $tipo_usuario;
+
+                // Almacena el tipo de usuario en una cookie
+                setcookie("tipo_usuario", $tipo_usuario, time() + (86400 * 30), "/"); // Caduca en 30 días
+
+                // Crea una cookie llamada "id_usuario" con el valor correspondiente a la columna "Usuario"
+                setcookie("id_usuario", $result["usuario"], time() + (86400 * 30), "/");
+
+                switch ($tipo_usuario) {
+                    case "Admin":
+                        header("Location: ../index2.php");
+                        break;
+                    case "Usuario":
+                        header("Location: ../index2.php");
+                        break;
+                    default:
+                        header("Location: ../index2.php");
+                        break;
                 }
             } else {
                 $_SESSION["mensaje"] = 'Acceso Denegado';
