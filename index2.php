fe47a5cbc69953ba961bf4ed5f24e06aeda965a6
José Ramírez <jwh743@gmail.com>
2024-02-22 11:17:29 -0300

    Se cambió la tag del título de la página

--- index2.php
+++ index2.php
@@ -46,7 +46,7 @@ $stmt->execute();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width">
-    <title>Galeria de fotos A.J</title>
+    <title>Galeria de fotos A.J.V.</title>
     <link rel="icon" type="image/x-icon" href="assets/logovierciblanco.svg">
     <!-- Librerías -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
