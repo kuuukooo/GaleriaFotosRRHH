697ac6b98112945de9d6ee0bdf789634112809b0
José Ramírez <jwh743@gmail.com>
2024-03-11 10:32:36 -0300

    Se logró reversear el array para traer los datos del más reciente al más antiguo

--- Albums/scriptGeneraciónImágenes.js
+++ Albums/scriptGeneraciónImágenes.js
@@ -14,53 +14,61 @@ function CargadeImagenes() {
     // Agregar el div al cuerpo del documento
     $(galeriaContainer).append(galleryDiv);
 
-    // Realizar petición AJAX para obtener los datos de los álbumes e imágenes
-    $.ajax({
-        url: 'datosImagenes.php',
-        dataType: 'json',
-        success: function(data) {
-            let items = [];
-        
-            // Iterar sobre los datos ordenados y construir los objetos de la galería
-            $.each(data, function(index, album) {
-                // Añadir el álbum
-                items.push({
-                    src: "fotos/" + album.miniatura,
-                    srct: "fotos/" + album.miniatura,
-                    title: album.descripcion,
-                    ID: album.id_album,
-                    kind: 'album',
-                    customData: {
-                        date: album.fecha_creacion // Asegúrate de que esta propiedad está asignada correctamente
-                    }
-                });
-        
-                // Añadir las imágenes del álbum ordenadas por fecha de creación
-                $.each(album.imagenes, function(index, imagen) {
-                    items.push({
-                        src: "fotos/" + imagen.imagen,
-                        albumID: album.id_album
-                    });
-                });
-            });
-            console.log(items);
+// Realizar petición AJAX para obtener los datos de los álbumes e imágenes
+$.ajax({
+    url: 'datosImagenes.php',
+    dataType: 'json',
+    success: function(data) {
+        // Convertir el objeto en un array
+        var dataArray = Object.values(data);
 
-            // Inicializar la galería nanogallery2 con los items obtenidos
-            $("#my_nanogallery2").nanogallery2({
-                items: items,
-                thumbnailWidth: 300,
-                thumbnailHeight: 300,
-                thumbnailAlignment: 'center',
-                thumbnailGutterWidth: 70,
-                thumbnailGutterHeight: 50,
-                galleryMaxRows: 3,
-                locationHash: false
+        // Toda la fé puesta.
+        dataArray.reverse();
+
+        let items = [];
+
+        console.log(dataArray)
+
+        // Iterar sobre los datos y construir los objetos de la galería
+        $.each(dataArray, function(index, album) {
+            // Añadir el álbum
+            items.push({
+                src: "fotos/" + album.miniatura,
+                srct: "fotos/" + album.miniatura,
+                title: album.descripcion,
+                ID: album.id_album,
+                kind: 'album',
+                customData: {
+                    date: album.fecha_creacion // Asegúrate de que esta propiedad está asignada correctamente
+                }
             });
-        }, 
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.error('Error al obtener los datos de los álbumes:', textStatus, errorThrown);
-        }
-    });
+
+            // Añadir las imágenes del álbum
+            $.each(album.imagenes, function(index, imagen) {
+                items.push({
+                    src: "fotos/" + imagen.imagen,
+                    albumID: album.id_album
+                });
+            });
+
+        });
+
+        // Inicializar la galería nanogallery2 con los items obtenidos
+        $("#my_nanogallery2").nanogallery2({
+            items: items,
+            thumbnailWidth: 300,
+            thumbnailHeight: 300,
+            thumbnailAlignment: 'center',
+            thumbnailGutterWidth: 70,
+            thumbnailGutterHeight: 50,
+            galleryMaxRows: 3,
+            locationHash: false
+        });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.error('Error al obtener los datos de los álbumes:', textStatus, errorThrown);
+    }
+}); 
 }
 
 
