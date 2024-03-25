a89eef0fcfb0976f9e7d32900f0a575af26f0403
José Ramírez <jwh743@gmail.com>
2024-03-25 14:20:01 -0400

    Se agregó la lógica de manejo de botones, la lógica de descarga, la lógica de eliminar y editar. Se reestructuró el código y se borraron partes que no servían.

--- Albums/scriptGeneraciónImágenes.js
+++ Albums/scriptGeneraciónImágenes.js
@@ -1,109 +1,109 @@
-//Event Listener que controla el click para cerrar el dialog 
-const dialog = document.querySelector('dialog');
-const wrapper = document.querySelector('.wrapper');
-
-dialog.addEventListener("click", (e) => !wrapper.contains(e.target) && dialog.close());
-
-console.log("Corriendo prueba.");
-
-//Función para mostrar el dialog
-function showDialog(show) {
+    //Event Listener que controla el click para cerrar el dialog 
     const dialog = document.querySelector('dialog');
-    show ? dialog.showModal() : dialog.close(), limpiar();
-    }
-    
-const CargadeImagenes = () => {
-    // Crear el div de my_nanogallery2 con los atributos data necesarios
-    let galleryDiv = $('<div id="my_nanogallery2" data-nanogallery2=\'{}\'></div>');
-    let galeriaContainer = document.getElementById('galeriaContainer');
-    // Agregar el div al cuerpo del documento
-    $(galeriaContainer).append(galleryDiv);
+    const wrapper = document.querySelector('.wrapper');
 
-    // Realizar petición AJAX para obtener los datos de los álbumes e imágenes
-    $.ajax({
-        url: 'datosImagenes.php',
-        dataType: 'json',
-        success: function(data) {
-            // Convertir el objeto en un array
-            var dataArray = Object.values(data);
+    dialog.addEventListener("click", (e) => !wrapper.contains(e.target) && dialog.close());
 
-            // Toda la fé puesta.
-            dataArray.reverse();
+    console.log("Corriendo prueba.");
 
-            let items = [];
-
-            console.log(dataArray)
-
-            // Iterar sobre los datos y construir los objetos de la galería
-            $.each(dataArray, function(index, album) {
-                // Añadir el álbum
-                items.push({
-                    src: "fotos/" + album.miniatura,
-                    srct: "fotos/" + album.miniatura,
-                    title: album.descripcion,
-                    ID: album.id_album,
-                    kind: 'album',
-                    customData: {
-                        date: album.fecha_creacion,
-                        AlbumID: album.id_album,
-                    }
-                });
-
-                // Añadir las imágenes del álbum
-                $.each(album.imagenes, function(index, imagen) {
-                    items.push({
-                        src: "fotos/" + imagen.imagen,
-                        albumID: album.id_album
-                    });
-                });
-
-            });
-
-        // Inicializar la galería nanogallery2 con los items obtenidos
-        $("#my_nanogallery2").nanogallery2({
-            items: items,
-            thumbnailWidth: 300,
-            thumbnailHeight: 300,
-            thumbnailAlignment: 'center',
-            thumbnailGutterWidth: 70,
-            thumbnailGutterHeight: 50,
-            galleryMaxRows: 3,
-            galleryDisplayMode: 'pagination',
-            galleryPaginationMode: 'numbers',
-            locationHash: false,
-            //Custom1: Borrar Album, Custom2: Cambiar el título del Album, custom3: Descargar Album
-            thumbnailToolbarAlbum: { topLeft: 'custom1, custom2, custom3' }, 
-            thumbnailToolbarImage: { topLeft: 'download' }, //Herramienta de descarga dentro de las imágenes
-            icons: {
-                thumbnailCustomTool1: '<i class="bi bi-trash" style="color: white"></i>', //Borrar Album
-                thumbnailCustomTool2: '<i class="bi bi-pencil-square"></i>', //Cambiar el título del Album
-                thumbnailCustomTool3: '<i class="bi bi-download" style="color: white ;"></i>' //Descargar Album
-            },
-            fnThumbnailToolCustAction: myTnTool // Cambiar la función que maneja las acciones personalizadas
-        });
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.error('Error al obtener los datos de los álbumes:', textStatus, errorThrown);
+    //Función para mostrar el dialog
+    function showDialog(show) {
+        const dialog = document.querySelector('dialog');
+        show ? dialog.showModal() : dialog.close(), limpiar();
         }
-    }); 
-}
+        
+    const CargadeImagenes = () => {
+        // Crear el div de my_nanogallery2 con los atributos data necesarios
+        let galleryDiv = $('<div id="my_nanogallery2" data-nanogallery2=\'{}\'></div>');
+        let galeriaContainer = document.getElementById('galeriaContainer');
+        // Agregar el div al cuerpo del documento
+        $(galeriaContainer).append(galleryDiv);
 
-// Función para manejar acciones personalizadas en la herramienta de miniaturas
-function myTnTool(action, item) {
-    console.dir(item);
+        // Realizar petición AJAX para obtener los datos de los álbumes e imágenes
+        $.ajax({
+            url: 'datosImagenes.php',
+            dataType: 'json',
+            success: function(data) {
+                // Convertir el objeto en un array
+                var dataArray = Object.values(data);
 
-    switch (action) {
-        case 'custom1':
-            BorrarAlbum(item);
-            break;
-        case 'custom2':
-            //Lógica para editar la descripción del Album.
-            break;
-        case 'custom3':
-            descargarAlbum(item); // Pasar el AlbumID como 'albumID'
-            break;
+                // Toda la fé puesta.
+                dataArray.reverse();
+
+                let items = [];
+
+                console.log(dataArray)
+
+                // Iterar sobre los datos y construir los objetos de la galería
+                $.each(dataArray, function(index, album) {
+                    // Añadir el álbum
+                    items.push({
+                        src: "fotos/" + album.miniatura,
+                        srct: "fotos/" + album.miniatura,
+                        title: album.descripcion,
+                        ID: album.id_album,
+                        kind: 'album',
+                        customData: {
+                            date: album.fecha_creacion,
+                            AlbumID: album.id_album,
+                        }
+                    });
+
+                    // Añadir las imágenes del álbum
+                    $.each(album.imagenes, function(index, imagen) {
+                        items.push({
+                            src: "fotos/" + imagen.imagen,
+                            albumID: album.id_album
+                        });
+                    });
+
+                });
+
+            // Inicializar la galería nanogallery2 con los items obtenidos
+            $("#my_nanogallery2").nanogallery2({
+                items: items,
+                thumbnailWidth: 300,
+                thumbnailHeight: 300,
+                thumbnailAlignment: 'center',
+                thumbnailGutterWidth: 70,
+                thumbnailGutterHeight: 50,
+                galleryMaxRows: 3,
+                galleryDisplayMode: 'pagination',
+                galleryPaginationMode: 'numbers',
+                locationHash: false,
+                //Custom1: Borrar Album, Custom2: Cambiar el título del Album, custom3: Descargar Album
+                thumbnailToolbarAlbum: { topLeft: 'custom1, custom2, custom3' }, 
+                thumbnailToolbarImage: { topLeft: 'download' }, //Herramienta de descarga dentro de las imágenes
+                icons: {
+                    thumbnailCustomTool1: '<i class="bi bi-trash" style="color: white"></i>', //Borrar Album
+                    thumbnailCustomTool2: '<i class="bi bi-pencil-square"></i>', //Cambiar el título del Album
+                    thumbnailCustomTool3: '<i class="bi bi-download" style="color: white ;"></i>' //Descargar Album
+                },
+                fnThumbnailToolCustAction: myTnTool // Cambiar la función que maneja las acciones personalizadas
+            });
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('Error al obtener los datos de los álbumes:', textStatus, errorThrown);
+            }
+        }); 
     }
-}
+
+    // Función para manejar acciones personalizadas en la herramienta de miniaturas
+    function myTnTool(action, item) {
+        console.dir(item);
+
+        switch (action) {
+            case 'custom1':
+                BorrarAlbum(item);
+                break;
+            case 'custom2':
+                EditarDescripcionAlbum(item);
+                break;
+            case 'custom3':
+                descargarAlbum(item); 
+                break;
+        }
+    }
 
     //Función para crear el álbum
     const crearAlbum = () => {
@@ -170,15 +170,6 @@ function myTnTool(action, item) {
         $('#imagenInputDialogAlbum').val('');  // Borrar el valor del input de imágenes
     }
         
-
-    $('.cancelFooterDialogAlbum', '.spanHeader').on('click', function() {
-        limpiar();
-    });
-
-    // Llamar a la función para crear un álbum cuando se hace clic en el botón "Subir Imagen"
-    $('.saveFooterDialogAlbum').on('click', function() {
-        crearAlbum();
-    });
 
     //Función para Buscar Imágenes dentro de los álbumes
     function BuscarImg() {
@@ -252,8 +243,6 @@ function myTnTool(action, item) {
         });        
     }
     
-    BuscarImg();
-    
     function BorrarAlbum(item) {
         // Obtener el AlbumID del customData del objeto item
         var albumID = item.customData.AlbumID;
@@ -292,95 +281,128 @@ function myTnTool(action, item) {
         }
     }
     
-    function descargarAlbum(item) {
+    async function descargarAlbum(item) {
         console.log("Item:", item);
     
-        // Realizar la solicitud AJAX para obtener las imágenes del álbum
         $.ajax({
-            url: 'descargarAlbum.php', // Ruta del script PHP que devuelve las imágenes del álbum
+            url: 'descargarAlbum.php',
             type: 'POST',
             data: { albumID: item.customData.AlbumID },
-            dataType: 'json', // Esperamos recibir datos JSON
-            success: function(response) {
+            dataType: 'json',
+            success: async function(response) {
                 console.log("Respuesta de obtener imágenes del álbum:", response);
-                
+    
                 if (response.error) {
                     alert("Error al obtener las imágenes del álbum: " + response.error);
                     return;
                 }
-                
-                // Crear una nueva instancia de JSZip
-                var zip = new JSZip();
-                
-                // Agregar cada imagen al archivo ZIP
-                response.imagenes.forEach(function(imagen, index) {
-                    // Decodificar la imagen base64
-                    var binaryData = atob(imagen.imagen);
-                    var arrayBuffer = new ArrayBuffer(binaryData.length);
-                    var uint8Array = new Uint8Array(arrayBuffer);
-                    for (var i = 0; i < binaryData.length; i++) {
-                        uint8Array[i] = binaryData.charCodeAt(i);
-                    }
     
-                    // Agregar la imagen al ZIP
-                    zip.file('imagen' + index + '.jpg', uint8Array, { binary: true });
-                });
+                const zip = new JSZip();
+                const formData = new FormData();
     
-                // Generar el archivo ZIP
-                zip.generateAsync({ type: 'blob' }).then(function(content) {
-                    // Crear una URL para el Blob
-                    var url = window.URL.createObjectURL(content);
+                // Promesas para cargar todas las imágenes
+                const imagePromises = response.imagenes.map(async function(imagen) {
+                    // Separar las imágenes por comas
+                    const imagenes = imagen.imagen.split(',');
     
-                    // Crear un elemento <a> para descargar el archivo ZIP
-                    var a = document.createElement('a');
-                    a.href = url;
-                    a.download = 'album.zip';
+                    // Iterar sobre cada imagen y agregarla al ZIP
+                    await Promise.all(imagenes.map(async function(imagenNombre, index) {
+                        const imageBlob = await fetch(`/Galeria5-AJAX/Albums/fotos/${imagenNombre}`).then(res => {
+                            console.log(`Imagen cargada: ${imagenNombre}`);    
+                            return res.blob();
+                        });
     
-                    // Agregar el elemento <a> al DOM y simular un clic para iniciar la descarga
-                    document.body.appendChild(a);
-                    a.click();
+                        // Agregar la imagen al archivo ZIP en el cliente
+                        zip.file(imagenNombre, imageBlob);
+                        console.log(`Imagen agregada al ZIP: ${imagenNombre}`);
     
-                    // Eliminar el elemento <a> y liberar el URL del Blob
-                    document.body.removeChild(a);
+                        // Agregar la descripción de la imagen al FormData
+                        formData.append("image_descriptions[]", imagen.descripcion);
+                        console.log(`Descripción de la imagen agregada al FormData: ${imagen.descripcion}`);
+                    }));
+                }); 
+    
+                // Esperar a que todas las promesas se resuelvan
+                await Promise.all(imagePromises);
+    
+                // Generar el archivo ZIP en el cliente y descargarlo
+                zip.generateAsync({ type: "blob" }).then(function(content) {
+                    // Configurar el elemento de anclaje para descargar el archivo ZIP
+                    const anchor = document.createElement("a");
+                    const url = window.URL.createObjectURL(content);
+                    anchor.href = url;
+                    anchor.download = `Album_${item.title}.zip`;
+                    console.log("Nombre de archivo descargado:", `album_${item.customData.AlbumID}.zip`);
+    
+                    // Simular el clic en el elemento de anclaje para iniciar la descarga
+                    anchor.click();
+    
+                    // Liberar el objeto URL
                     window.URL.revokeObjectURL(url);
                 });
             },
             error: function(xhr, status, error) {
                 console.error(xhr.responseText);
-                console.log(item.data);
                 alert("Error en la solicitud AJAX para obtener las imágenes del álbum. Consulta la consola para más detalles.");
             }
         });
     }
     
+    const  EditarDescripcionAlbum = (item) => {
+        // Obtener el ID del álbum del objeto item (suponiendo que item contiene el ID del álbum)
+        let  albumID = item.customData.AlbumID;
+        console.log(albumID);
     
-
-    function EliminarImg() {
-        // Cambiar entre el modo para seleccionar imágenes a eliminar y el modo normal
-        $('#eliminarAlb').on('click', function(e) {
-            e.preventDefault();
+        // Solicitar al usuario que ingrese la nueva descripción del álbum
+        var nuevaDescripcion = prompt("Ingrese la nueva descripción del álbum:");
     
-            // Obtiene el estado actual de 'thumbnailSelectable'
-            let selectable = $('#my_nanogallery2').nanogallery2('option', 'thumbnailSelectable');
+        if (nuevaDescripcion !== null && nuevaDescripcion.trim() !== '') {
+            // Crear un objeto FormData para enviar los datos al backend
+            let formData = new FormData();
+            formData.append('albumId', albumID);
+            formData.append('newDescription', nuevaDescripcion);
     
-            // Alterna entre true y false
-            selectable = !selectable;
-    
-            // Establece el nuevo valor de 'thumbnailSelectable'
-            $('#my_nanogallery2').nanogallery2('option', 'thumbnailSelectable', selectable);
-    
-            // Cambia el texto del botón según el estado de 'selectable'
-            if (selectable) {
-                $(this).text("Seleccione un álbum");
-            } else {
-                $(this).text("Eliminar un álbum");
-            }
-        });
+            // Realizar la solicitud AJAX para actualizar la descripción del álbum
+            $.ajax({
+                url: 'editarDescripcion.php',
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    console.log("Respuesta exitosa:", response);
+                    if (response.success) {
+                        alert("Descripción del álbum actualizada correctamente.");
+                        $(galeriaContainer).empty();
+                        CargadeImagenes();
+                    } else if (response.error) {
+                        alert("Error al actualizar la descripción del álbum: " + response.error);
+                    } else {
+                        alert("Error inesperado en la respuesta del servidor.");
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                    alert("Error en la solicitud AJAX. Consulta la consola para más detalles.");
+                }
+            });
+        } else {
+            alert("La descripción no puede estar vacía.");
+        }
     }
     
-    EliminarImg();
+        
+    BuscarImg();
 
-    
     $(document).ready(function () {
         CargadeImagenes();
+    });
+    
+    $('.cancelFooterDialogAlbum', '.spanHeader').on('click', function() {
+        limpiar();
+    });
+
+    // Llamar a la función para crear un álbum cuando se hace clic en el botón "Subir Imagen"
+    $('.saveFooterDialogAlbum').on('click', function() {
+        crearAlbum();
     });
