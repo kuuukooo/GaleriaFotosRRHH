fe379d9db792c508997c54e3b111182dcbbc7ef4
José Ramírez <jwh743@gmail.com>
2024-04-02 10:11:17 -0400

    Se agregó una carpeta para una mejor organización

--- index2.php
+++ index2.php
@@ -57,7 +57,7 @@ $stmt->execute();
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="estilos.css">
-    <link rel="stylesheet" type="text/css" href="navbar2.css">
+    <link rel="stylesheet" type="text/css" href="NavBar/navbar2.css">
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
@@ -204,7 +204,7 @@ $stmt->execute();
 <script src="imagenesDinámicas.js"></script>
 <script src="descarga_imágenes.js"></script>
 <script src="eliminar_imagen.js"></script>
-<script src="navbar2.js"></script>
+<script src="NavBar/navbar2.js"></script>
 <script src="login/ScriptTipoUsuario.js"></script> 
 </body>
 </html>
