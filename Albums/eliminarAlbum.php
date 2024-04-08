3bd36d5dbc2d1698cc2410340e1b8128d874b988
José Ramírez <jwh743@gmail.com>
2024-04-08 11:42:15 -0400

    Se agregaron líneas de código para borrar las imágenes del directorio

--- Albums/eliminarAlbum.php
+++ Albums/eliminarAlbum.php
@@ -17,7 +17,34 @@ try {
         $conn->beginTransaction();
 
         try {
-            // Eliminar las imágenes asociadas al álbum
+            // Obtener las imágenes asociadas al álbum
+            $stmt = $conn->prepare("SELECT imagen FROM imagenes_albumes WHERE id_album = :albumID");
+            $stmt->bindParam(':albumID', $albumID, PDO::PARAM_INT);
+            $stmt->execute();
+            $imagesString = $stmt->fetchColumn();
+
+            // Dividir la cadena de imágenes en un array
+            $images = explode(",", $imagesString);
+
+            // Eliminar las imágenes asociadas al álbum del directorio
+            foreach ($images as $image) {
+                $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/Galeria5-AJAX/Albums/fotos/' . trim($image);
+
+                if (file_exists($imagePath)) {
+                    if (unlink($imagePath)) {
+                        // Debuggeo: Mostrar que la imagen fue eliminada
+                        //echo json_encode(array("debug" => "Imagen eliminada correctamente: " . $imagePath));
+                    } else {
+                        // Debuggeo: Mostrar si hubo un error al eliminar la imagen
+                        echo json_encode(array("debug" => "Error al eliminar la imagen: " . $imagePath));
+                    }
+                } else {
+                    // Debuggeo: Mostrar si la imagen no existe en la ruta especificada
+                    echo json_encode(array("debug" => "La imagen no existe en la ruta especificada: " . $imagePath));
+                }
+            }
+
+            // Eliminar las imágenes asociadas al álbum de la base de datos
             $stmt = $conn->prepare("DELETE FROM imagenes_albumes WHERE id_album = :albumID");
             $stmt->bindParam(':albumID', $albumID, PDO::PARAM_INT);
             $stmt->execute();
@@ -43,4 +70,3 @@ try {
 } catch (PDOException $e) {
     echo json_encode(array("error" => "Error en la conexión a la base de datos: " . $e->getMessage()));
 }
-
