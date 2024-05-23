3c2810542af761c1d1d1a988bf67ca5d768fe74a
José Ramírez <jwh743@gmail.com>
2024-05-23 11:10:48 -0400

    Se agregó la lógica para ocultar la pestaña pública y el botón de publicar

--- login/ScriptTipoUsuario.js
+++ login/ScriptTipoUsuario.js
@@ -1,1 +1,75 @@
-function getCookie(e){return new Promise((t,o)=>{var n=("; "+document.cookie).split("; "+e+"=");2==n.length?t(n.pop().split(";").shift()):o("Cookie not found")})}async function checkCookie(){try{var e=await getCookie("tipo_usuario");if("Usuario"==e){for(var t=document.getElementsByClassName("delete-button"),o=0;o<t.length;o++)t[o].style.display="none";for(var n=document.getElementsByClassName("btn-edit-description"),o=0;o<n.length;o++)n[o].style.display="none";let i=document.querySelectorAll('[data-ngy2action="custom1"]');for(let l=0;l<i.length;l++)i[l].style.display="none";let s=document.querySelectorAll('[data-ngy2action="custom2"]');for(let a=0;a<s.length;a++)s[a].style.display="none"}else $("#DashboardMenu").show()}catch(r){console.error(r)}}function observeDOM(){var e;new MutationObserver(function(e){e.forEach(function(e){("childList"===e.type||"subtree"===e.type)&&checkCookie()})}).observe(document.body,{childList:!0,subtree:!0})}window.onload=function(){checkCookie(),observeDOM()};
+//Get the cookie "tipo_usuario" from the browser, based on the info given by the cookie
+//either "Admin" or "Usuario", if the given value is "Usuario" then the code gets the
+//document class .delete-button and removes it by setting its display to none
+//and also gets the document class .download-button and also removes it by setting its 
+//display to none, if the given value is "Admin" then the code does nothing.
+function getCookie(name) {
+    return new Promise((resolve, reject) => {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) resolve(parts.pop().split(";").shift());
+        else reject('Cookie not found');
+    });
+}
+
+async function checkCookie() {
+    try {
+        var tipo_usuario = await getCookie("tipo_usuario");
+        if (tipo_usuario == "Usuario") {
+            var deleteButton = document.getElementsByClassName("delete-button");
+
+            for (var i = 0; i < deleteButton.length; i++) {
+                deleteButton[i].style.display = "none";
+            }
+            var downloadButton = document.getElementsByClassName("btn-edit-description");
+            for (var i = 0; i < downloadButton.length; i++) {
+                downloadButton[i].style.display = "none";
+            }
+            var publicButton = document.getElementsByClassName("btn-publicar");
+            for (var i = 0; i < publicButton.length; i++) {
+                publicButton[i].style.display = "none";
+            }
+            let deleteButtonAlbum = document.querySelectorAll('[data-ngy2action="custom1"]');
+            for (let i = 0; i < deleteButtonAlbum.length; i++) {
+                deleteButtonAlbum[i].style.display = "none";
+            }
+            let editButtonAlbum = document.querySelectorAll('[data-ngy2action="custom2"]');
+            for (let i = 0; i < editButtonAlbum.length; i++){
+                editButtonAlbum[i].style.display = "none";
+            }
+            let publicButtonAlbum = document.querySelectorAll('[data-ngy2action="custom4"]');
+            for (let i = 0; i < publicButtonAlbum.length; i++){
+                publicButtonAlbum[i].style.display = "none";
+            }
+        } else {
+            $("#DashboardMenu").show();
+            $("#GaleriaPublica").show();
+        }
+
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function observeDOM() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList' || mutation.type === 'subtree') {
+                // Call checkCookie when the DOM changes
+                checkCookie();
+            }
+        });
+    });
+
+    var config = { childList: true, subtree: true };
+    observer.observe(document.body, config);
+}
+
+window.onload = function() {
+    // Call checkCookie immediately
+    checkCookie();
+
+    // Start observing the DOM for changes
+    observeDOM();
+}
